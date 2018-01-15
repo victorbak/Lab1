6039b8697f70fef89e2324439e3586415b43fb00
Victor Bak <victor.bak2@gmail.com>
2018-01-14 17:11:32 -0800

    3 students added, prints info and averages

--- Student.php
+++ Student.php
@@ -13,18 +13,36 @@
  */
 class Student {
     //put your code here
-    function __construc() {
+    function __construct() {
         $this->surname = '';
         $this->first_name  = '';
         $this->emails = array();
         $this->grades = array();
     }
     
-    function add_email($which,$address) {
-        $this->emails[$which = $address];
-    }
+function add_email($which,$address) {
+    $this->emails[$which] = $address;
+}
+
+function add_grade($grade) {
+    $this->grades[] = $grade;
+}
     
-    function add_grade($grade) {
-        $this->grades[] = $grade;
-    }
+//calculates student average
+function average() {
+    $total = 0;
+    foreach ($this->grades as $value)
+        $total += $value;
+    return $total / count($this->grades);
+}
+    
+//students to string function
+function toString() {
+    $result = $this->first_name . ' ' . $this->surname;
+    $result .= ' ('.$this->average().")\n";
+    foreach($this->emails as $which=>$what)
+        $result .= $which . ': '. $what. "\n";
+    $result .= "\n";
+    return '<pre>'.$result.'</pre>';
+}
 }
