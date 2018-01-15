2b24e64611a48280bfd785f73f86cb825dee2aec
Victor Bak <victor.bak2@gmail.com>
2018-01-14 17:16:41 -0800

    Index didn't push properly, nothing changed since last commit.

--- index.php
+++ index.php
@@ -10,8 +10,52 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-            echo "Hello World"
-        ?>
+<?php
+include('Student.php');
+
+//making array of students
+$students = array();
+
+//adding in first student
+$first = new Student();
+$first->surname = "Doe";
+$first->first_name = "John";
+$first->add_email('home','john@doe.com');
+$first->add_email('work','jdoe@mcdonalds.com');
+$first->add_grade(65);
+$first->add_grade(75);
+$first->add_grade(55);
+$students['j123'] = $first;
+
+//second student
+$second = new Student();
+$second->surname = "Einstein";
+$second->first_name = "Albert";
+$second->add_email('home','albert@braniacs.com');
+$second->add_email('work1','a_einstein@bcit.ca');
+$second->add_email('work2','albert@physics.mit.edu');
+$second->add_grade(95);
+$second->add_grade(80);
+$second->add_grade(50);
+$students['a456'] = $second;
+
+//third student
+$third = new Student();
+$third->surname = "Bak";
+$third->first_name = "Victor";
+$third->add_email('home','victor@braniacs.com');
+$third->add_grade(100);
+$third->add_grade(0);
+$third->add_grade(0);
+$students['c456'] = $third;
+
+//sort students
+ksort($students);
+
+//printing student info
+foreach($students as $student)
+    echo $student->toString();
+
+?>
     </body>
 </html>
